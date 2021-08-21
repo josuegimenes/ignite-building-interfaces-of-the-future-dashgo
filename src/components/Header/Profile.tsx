37749588430db9e86090409c83aab29a14df82e8
Josué Gimenes <josuegalvares@gmail.com>
2021-08-20 21:50:52 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,22 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Josué Gimenes</Text>
-            <Text
-              color="gray.300"
-              fontSize="small">
-                josuegalvares@gmail.com
-            </Text>
-          </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+        <Text>Josué Gimenes</Text>
+        <Text color="gray.300" fontSize="small">
+            josuegalvares@gmail.com
+        </Text>
+      </Box>
+      )}
 
-          <Avatar size="md" name="Josué Gimenes" src="https://github.com/josuegimenes.png" />
-        </Flex>
+      <Avatar size="md" name="Josué Gimenes" src="https://github.com/josuegimenes.png" />
+    </Flex>
   );
 }
